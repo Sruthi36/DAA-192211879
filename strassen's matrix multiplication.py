d826170f9eed87e44c6df73e9b9c81550731dbaa
Sruthi36 <124115664+Sruthi36@users.noreply.github.com>
2024-03-04 23:04:04 +0530

    Update strassen's matrix multiplication.py

--- strassen's matrix multiplication.py
+++ strassen's matrix multiplication.py
@@ -1,7 +1,26 @@
-def split(A,B):
-    if len(A == 1):
-        return A*B
-    else:
-        half = len(A)//2
-        split(A[:half],B[:half])
-A = 
+def strassen_multiply(x, y):
+    z = [[0, 0], [0, 0]]
+
+    m1 = (x[0][0] + x[1][1]) * (y[0][0] + y[1][1])
+    m2 = (x[1][0] + x[1][1]) * y[0][0]
+    m3 = x[0][0] * (y[0][1] - y[1][1])
+    m4 = x[1][1] * (y[1][0] - y[0][0])
+    m5 = (x[0][0] + x[0][1]) * y[1][1]
+    m6 = (x[1][0] - x[0][0]) * (y[0][0] + y[0][1])
+    m7 = (x[0][1] - x[1][1]) * (y[1][0] + y[1][1])
+
+    z[0][0] = m1 + m4 - m5 + m7
+    z[0][1] = m3 + m5
+    z[1][0] = m2 + m4
+    z[1][1] = m1 - m2 + m3 + m6
+
+    return z
+
+x = [[12, 34], [22, 10]]
+y = [[3, 4], [2, 1]]
+
+
+result = strassen_multiply(x, y)
+
+for row in result:
+    print(row)
